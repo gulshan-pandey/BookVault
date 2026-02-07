ae6620dfcb9f2bed08f85850fac1e2fbc1fc06da
gulshan-pandey <gulshanpandey7210@gmail.com>
2026-02-07 17:21:13 +0530

    Added swagger for api docs but in Authod controller

--- src/main/java/com/application/BookVault/controller/AuthorController.java
+++ src/main/java/com/application/BookVault/controller/AuthorController.java
@@ -9,9 +9,10 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
-@Controller
+@RestController
 public class AuthorController {
 
     @Autowired
